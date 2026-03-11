810dc61f9eaa451df83c07fbe829917f8e33ae6a
tanishkabhoir79-eng <tanishkabhoir79@gmail.com>
2026-03-11 19:39:37 +0530

    to print elements of list in a single line

--- element of list in single line.py
+++ element of list in single line.py
@@ -4,3 +4,9 @@ heroes=["thor","ironman","batman","shaktiman"]
 def print_len(list):
     print(len(list))
 
+def print_list(list):
+    for item in list:
+        print(item,end=" ")
+
+print_list(heroes)
+print()
